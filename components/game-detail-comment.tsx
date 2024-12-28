c1fe39fc28ef17966769af0c8f90f1a4b9c58f7f
chanho <dirtyface1234@naver.com>
2024-12-28 15:55:11 +0900

    Initialize subgraph

--- components/game-detail-comment.tsx
+++ components/game-detail-comment.tsx
@@ -52,22 +52,13 @@ useEffect(() => {
       <div className="flex items-center space-x-2">
         <Label>Description</Label>
         <Input id="description" placeholder="Please leave a comment." />
-        <Button className="w-200 h-110 cursor-pointer bg-[#00A29A] bg-lime-700 object-contain text-white transition-transform duration-75 hover:bg-[#00A29A] active:scale-95 active:bg-[#66C2B8]">
-          Up
-        </Button>
-        <Button className="w-200 h-110 cursor-pointer bg-[#C73535] bg-rose-700 object-contain text-white transition-transform duration-75 hover:bg-[#C73535] active:scale-95 active:bg-[#E57373]">
-          Down
+        <Button className="w-200 h-110 cursor-pointer bg-[#00A29A] bg-[rgb(105,227,169)] object-contain text-black transition-transform duration-75 hover:bg-[#00A29A] active:scale-95 active:bg-[#66C2B8]">
+          Comment
         </Button>
       </div>
       <div className="ml-5 mr-5 flex space-x-4">
         <div className="w-1/2 space-y-4">
           <div className="space-y-4">
-            <div className="mb-2 flex items-center">
-              <span className="mb-2 mr-5 flex items-center rounded-[15px] border border-[#00A29A] px-3 py-1 text-xs font-bold text-[#00A29A]">
-                Price Up
-              </span>
-              <span className="text-[11px] text-[#B6B6B6]">+213 Comment</span>
-            </div>
             <div className="grid gap-6">
               {walletAddresses.slice(0, 4).map((address, index) => (
                 <div
@@ -94,52 +85,8 @@ useEffect(() => {
           </div>
         </div>
 
-        <div className="w-1/2 space-y-4">
-          <div className="space-y-4">
-            <div className="mb-2 flex items-center">
-              <span className="mb-2 mr-5 flex items-center rounded-[15px] border border-[#C73535] px-3 py-1 text-xs font-bold text-[#C73535]">
-                Price Down
-              </span>
-              <span className="text-[11px] text-[#B6B6B6]">+98 Comment</span>
-            </div>
-            <div className="grid gap-6">
-              {walletAddresses.slice(4, 8).map((address, index) => (
-                <div
-                  key={index}
-                  className="flex items-center justify-between space-x-4"
-                >
-                  <div className="flex items-center space-x-4">
-                    <Avatar>
-                      <AvatarImage src="/avatars/03.png" alt="Image" />
-                      <AvatarFallback>OM</AvatarFallback>
-                    </Avatar>
-                    <div>
-                      <p className="text-xs text-muted-foreground">
-                        {shortenAddress(address)}
-                      </p>
-                      <p className="text-sm font-medium leading-none">
-                        Hey its not real
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              ))}
-            </div>
-          </div>
-        </div>
         
       </div>
-      <Button className='mt-6'>
-      <Link href={"https://testnets.opensea.io/assets/sepolia/" + address + "/1"} >
-        Up Vote NFT Link
-        </Link>
-      </Button>
-      <br/>
-      <Button className='mt-6'>
-      <Link href={"https://testnets.opensea.io/assets/sepolia/" + address + "/2"} >
-        Down Vote NFT Link
-        </Link>
-      </Button>
     </div>
   );
 };

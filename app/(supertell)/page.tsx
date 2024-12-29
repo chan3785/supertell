bd9ef4eb0a5cd8fdc41c7cf358e092006b3f0701
chanho <dirtyface1234@naver.com>
2024-12-29 19:11:10 +0900

    mobile screen

--- app/(supertell)/page.tsx
+++ app/(supertell)/page.tsx
@@ -1,13 +1,17 @@
+'use client';
 import { GameList } from '@/components/game-list';
+import { useResponsive } from '@/hooks/useResponsive';
 
-export default function page() {
+export default function Page() {
+  const { isMobile } = useResponsive();
+
   return (
-    <div className="mt-10 space-y-6 p-4 md:p-8 overflow-y-scroll h-full max-h-[calc(100vh-100px)]">
-      <h2 className="scroll-m-20 text-4xl font-bold tracking-tight lg:text-3xl">
+    <div className={`space-y-6 p-4 md:p-8 overflow-y-scroll h-full max-h-[calc(100vh-100px)] ${isMobile ? 'mt-40' : 'mt-10'}`}>
+      <h2 className={`scroll-m-20 font-bold tracking-tight ${isMobile ? 'text-2xl' : 'text-4xl lg:text-3xl'}`}>
         SuperTell Price Prediction
       </h2>
-      <h1 className="mb-20 scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-      Get precise news and predict the price
+      <h1 className={`mb-20 scroll-m-20 font-extrabold tracking-tight ${isMobile ? 'text-3xl' : 'text-4xl lg:text-5xl'}`}>
+        Get precise news and predict the price
       </h1>
       <div>
         <GameList/>
--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -9,7 +9,7 @@ export default function Header() {
 
   return (
     <div className="supports-backdrop-blur:bg-background/60 fixed left-0 right-0 top-0 z-20 border-b bg-background/95 backdrop-blur">
-      <nav className={`ml-20 mr-20 flex items-center justify-between px-4 ${isMobile ? 'flex-col h-40' : 'flex-row h-20'}`}>
+      <nav className={`ml-20 mr-20 flex items-center justify-between px-4 ${isMobile ? 'flex-col h-75' : 'flex-row h-20'}`}>
         <div className={`flex items-center ${isMobile ? 'space-y-2 flex-col' : 'space-x-4 flex-row'}`}>
           <Link href={'/'}>
             <Button className={`w-${isMobile ? '[30px]' : '[50px]'} ml-7 h-${isMobile ? '[10px]' : '[15px]'} font-bold`} variant="ghost">
